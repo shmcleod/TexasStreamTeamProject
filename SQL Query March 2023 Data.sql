3b6710a15c6e96456dda78ca65be00b61be42054
shmcleod <130265683+shmcleod@users.noreply.github.com>
2023-04-19 05:36:56 -0500

    Update SQL Query March 2023 Data.sql

--- SQL Query March 2023 Data.sql
+++ SQL Query March 2023 Data.sql
@@ -1,3 +1,5 @@
+-- Skills used: Create View, Window Functions,
+
 -- Selecting All from Datamap
 
 SELECT * FROM TST..Datamap
@@ -13,11 +15,11 @@ ORDER BY [Nitrate-Nitrogen (ppm or mg L)] DESC
 -- Removing Data Errors
 
 SELECT * FROM TST..Datamap
-WHERE [Conductivity (µs cm)] <> 'Data error'
+WHERE [Conductivity (Âµs cm)] <> 'Data error'
 AND pH <> 'Data error'
 AND [Dissolved Oxygent (mg L)] <> 'Data error'
-AND [Air Temperature (°C)] <> 'Data error'
-AND [Water Temperature (°C)] <> 'Data error'
+AND [Air Temperature (Â°C)] <> 'Data error'
+AND [Water Temperature (Â°C)] <> 'Data error'
 AND [Nitrate-Nitrogen (ppm or mg L)] <> 'Data error'
 -- No Data Errors in Ecoli but adding for future
 AND [E  Coli Average] <> 'Data error'
@@ -27,11 +29,11 @@ ORDER BY sample_date ASC
 -- Finding Errors
 
 SELECT * FROM TST..Datamap
-WHERE [Conductivity (µs cm)] = 'Data error'
+WHERE [Conductivity (Âµs cm)] = 'Data error'
 OR pH = 'Data error'
 OR [Dissolved Oxygent (mg L)] = 'Data error'
-OR [Air Temperature (°C)] = 'Data error'
-OR [Water Temperature (°C)] = 'Data error'
+OR [Air Temperature (Â°C)] = 'Data error'
+OR [Water Temperature (Â°C)] = 'Data error'
 OR [Nitrate-Nitrogen (ppm or mg L)] = 'Data error'
 -- No Data Errors in Ecoli but adding for future
 OR [E  Coli Average] = 'Data error'
@@ -39,11 +41,11 @@ OR [E  Coli Average] = 'Data error'
 -- How many rows have errors? (Need to Fix these sites)
 
 SELECT COUNT(*) as Num_Samples_With_Errors FROM TST..Datamap
-WHERE [Conductivity (µs cm)] = 'Data error'
+WHERE [Conductivity (Âµs cm)] = 'Data error'
 OR pH = 'Data error'
 OR [Dissolved Oxygent (mg L)] = 'Data error'
-OR [Air Temperature (°C)] = 'Data error'
-OR [Water Temperature (°C)] = 'Data error'
+OR [Air Temperature (Â°C)] = 'Data error'
+OR [Water Temperature (Â°C)] = 'Data error'
 OR [Nitrate-Nitrogen (ppm or mg L)] = 'Data error'
 -- No Data Errors in Ecoli but adding for future
 OR [E  Coli Average] = 'Data error'
@@ -52,26 +54,26 @@ OR [E  Coli Average] = 'Data error'
 -- Creating Rolling Average for Parameters by Basin
 
 SELECT sample_date, Basin,
-[Conductivity (µs cm)],
-AVG(NULLIF(CAST([Conductivity (µs cm)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Cond_Average,
+[Conductivity (Âµs cm)],
+AVG(NULLIF(CAST([Conductivity (Âµs cm)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Cond_Average,
 pH,
 AVG(NULLIF(CAST(pH AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as pH_Average,
 [Dissolved Oxygent (mg L)],
 AVG(NULLIF(CAST([Dissolved Oxygent (mg L)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as DO_Average,
-[Air Temperature (°C)],
-AVG(NULLIF(CAST([Air Temperature (°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Air_temp_Average,
-[Water Temperature (°C)],
-AVG(NULLIF(CAST([Water Temperature (°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Water_temp_Average,
+[Air Temperature (Â°C)],
+AVG(NULLIF(CAST([Air Temperature (Â°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Air_temp_Average,
+[Water Temperature (Â°C)],
+AVG(NULLIF(CAST([Water Temperature (Â°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Water_temp_Average,
 [E  Coli Average],
 AVG(NULLIF(CAST([E  Coli Average] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as E_coli_Average,
 [Nitrate-Nitrogen (ppm or mg L)],
 AVG(NULLIF(CAST([Nitrate-Nitrogen (ppm or mg L)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Nitrate_Average
 FROM TST..Datamap
-WHERE [Conductivity (µs cm)] <> 'Data error'
+WHERE [Conductivity (Âµs cm)] <> 'Data error'
 AND pH <> 'Data error'
 AND [Dissolved Oxygent (mg L)] <> 'Data error'
-AND [Air Temperature (°C)] <> 'Data error'
-AND [Water Temperature (°C)] <> 'Data error'
+AND [Air Temperature (Â°C)] <> 'Data error'
+AND [Water Temperature (Â°C)] <> 'Data error'
 AND [Nitrate-Nitrogen (ppm or mg L)] <> 'Data error'
 -- No Data Errors in Ecoli but adding for future
 AND [E  Coli Average] <> 'Data error'
@@ -81,26 +83,26 @@ ORDER BY Basin, sample_date ASC
 -- Creating Rolling Average for parameters by Site_id
 
 SELECT sample_date, site_id, Basin,
-[Conductivity (µs cm)],
-AVG(NULLIF(CAST([Conductivity (µs cm)] AS float),0)) OVER (PARTITION BY site_id ORDER BY sample_date, site_id) as Cond_Average,
+[Conductivity (Âµs cm)],
+AVG(NULLIF(CAST([Conductivity (Âµs cm)] AS float),0)) OVER (PARTITION BY site_id ORDER BY sample_date, site_id) as Cond_Average,
 pH,
 AVG(NULLIF(CAST(pH AS float),0)) OVER (PARTITION BY site_id ORDER BY sample_date, site_id) as pH_Average,
 [Dissolved Oxygent (mg L)],
 AVG(NULLIF(CAST([Dissolved Oxygent (mg L)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as DO_Average,
-[Air Temperature (°C)],
-AVG(NULLIF(CAST([Air Temperature (°C)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as Air_temp_Average,
-[Water Temperature (°C)],
-AVG(NULLIF(CAST([Water Temperature (°C)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as Water_temp_Average,
+[Air Temperature (Â°C)],
+AVG(NULLIF(CAST([Air Temperature (Â°C)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as Air_temp_Average,
+[Water Temperature (Â°C)],
+AVG(NULLIF(CAST([Water Temperature (Â°C)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as Water_temp_Average,
 [E  Coli Average],
 AVG(NULLIF(CAST([E  Coli Average] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as E_coli_Average,
 [Nitrate-Nitrogen (ppm or mg L)],
 AVG(NULLIF(CAST([Nitrate-Nitrogen (ppm or mg L)] AS float),0)) OVER (PARTITION BY site_id  ORDER BY sample_date, site_id) as Nitrate_Average
 FROM TST..Datamap
-WHERE [Conductivity (µs cm)] <> 'Data error'
+WHERE [Conductivity (Âµs cm)] <> 'Data error'
 AND pH <> 'Data error'
 AND [Dissolved Oxygent (mg L)] <> 'Data error'
-AND [Air Temperature (°C)] <> 'Data error'
-AND [Water Temperature (°C)] <> 'Data error'
+AND [Air Temperature (Â°C)] <> 'Data error'
+AND [Water Temperature (Â°C)] <> 'Data error'
 AND [Nitrate-Nitrogen (ppm or mg L)] <> 'Data error'
 -- No Data Errors in Ecoli but adding for future
 AND [E  Coli Average] <> 'Data error'
@@ -117,31 +119,31 @@ WHERE sample_date < '1980-01-01'
 
 CREATE VIEW Rolling_Avg_Per_Basin AS
 SELECT sample_date, Basin,
-[Conductivity (µs cm)],
-AVG(NULLIF(CAST([Conductivity (µs cm)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Cond_Average,
+[Conductivity (Âµs cm)],
+AVG(NULLIF(CAST([Conductivity (Âµs cm)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Cond_Average,
 pH,
 AVG(NULLIF(CAST(pH AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as pH_Average,
 [Dissolved Oxygent (mg L)],
 AVG(NULLIF(CAST([Dissolved Oxygent (mg L)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as DO_Average,
-[Air Temperature (°C)],
-AVG(NULLIF(CAST([Air Temperature (°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Air_temp_Average,
-[Water Temperature (°C)],
-AVG(NULLIF(CAST([Water Temperature (°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Water_temp_Average,
+[Air Temperature (Â°C)],
+AVG(NULLIF(CAST([Air Temperature (Â°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Air_temp_Average,
+[Water Temperature (Â°C)],
+AVG(NULLIF(CAST([Water Temperature (Â°C)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Water_temp_Average,
 [E  Coli Average],
 AVG(NULLIF(CAST([E  Coli Average] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as E_coli_Average,
 [Nitrate-Nitrogen (ppm or mg L)],
 AVG(NULLIF(CAST([Nitrate-Nitrogen (ppm or mg L)] AS float),0)) OVER (PARTITION BY Basin ORDER BY sample_date, Basin) as Nitrate_Average
 FROM TST..Datamap
-WHERE [Conductivity (µs cm)] <> 'Data error'
+WHERE [Conductivity (Âµs cm)] <> 'Data error'
 AND pH <> 'Data error'
 AND [Dissolved Oxygent (mg L)] <> 'Data error'
-AND [Air Temperature (°C)] <> 'Data error'
-AND [Water Temperature (°C)] <> 'Data error'
+AND [Air Temperature (Â°C)] <> 'Data error'
+AND [Water Temperature (Â°C)] <> 'Data error'
 AND [Nitrate-Nitrogen (ppm or mg L)] <> 'Data error'
 -- No Data Errors in Ecoli but adding for future
 AND [E  Coli Average] <> 'Data error'
 AND sample_date <> '1900-01-01'
-AND [Conductivity (µs cm)] IS NOT NULL
+AND [Conductivity (Âµs cm)] IS NOT NULL
 
 SELECT * FROM Rolling_Avg_Per_Basin
 ORDER BY Basin, sample_date ASC
